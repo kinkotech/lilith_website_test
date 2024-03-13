4a4cbcea140bb09280cfd38f639274d9000c7714
haiyingshi <274976591@qq.com>
2024-03-13 14:17:44 +0800

    分享

--- src/views/m/share/index.vue
+++ src/views/m/share/index.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
+import { ref, reactive } from 'vue';
 import wx from 'weixin-js-sdk';
 import Clipboard from 'clipboard';
 import { showToast } from 'vant';
 import 'vant/es/toast/style';
 import api from '@/api';
 
+const state = reactive({
+    appId: '',
+    timestamp: 0,
+    nonceStr: '',
+    signature: '',
+	text: '邀请好友预约，免费得红包封面!-剑与远征:启程官方网站https://test-lilith.kinkotec.cn/',
+	desc: '就差你啦!助我领取《剑与远征:启程》新春红包封面点击链接[立即预约]完成...',
+    fxUrl: 'https://lilith.kinkotec.cn/?isInvitation=true',
+    title: '邀请好友预约，免费得红包封面!-剑与远征:启程官方网站',
+    fxImgUrl: 'https://test-lilith.kinkotec.cn/fx.png'
+})
 
-const text = '邀请好友预约，免费得红包封面!-剑与远征:启程官方网站https://test-lilith.kinkotec.cn/'
 const clipboard = new Clipboard('.copy-btn'); // .copy-btn为按钮元素的class名称或选择器
 
 clipboard.on('success', (e) => {
@@ -24,61 +35,69 @@ clipboard.on('error', () => {
     console.warn('无法复制到剪贴板！');
 });
 
-// 分享
-// let appId = 'wxa039b9f506b604a7';
-// let timestamp: any = '';
-// let nonceStr = '';
-// let signature = '';
-// let fxUrl = 'https://lilith.kinkotec.cn/?isInvitation=true';
-// let title = '邀请好友预约，免费得红包封面!-剑与远征:启程官方网站';
-// let fxImgUrl = 'https://test-lilith.kinkotec.cn/fx.png';
-// wx.config({
-//     debug: false,// 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-//     appId,         // 必填，公众号的唯一标识，填自己的！
-//     timestamp, // 必填，生成签名的时间戳，刚才接口拿到的数据
-//     nonceStr,   // 必填，生成签名的随机串
-//     signature, // 必填，签名，见附录1
-//     jsApiList: [
-//     'onMenuShareTimeline',
-//     'onMenuShareAppMessage'
-//     ]
-// })
+// 分享接口
+const getShareParam = async () => {
+    await api.getShare().then(res => {
+        console.log(res)
+        state.appId = res.APP_ID;
+        state.timestamp = res.timestamp;
+        state.nonceStr = res.nonceStr;
+        state.signature = res.signature;
+    })
+}
+await getShareParam()
 
-// wx.ready(function () {
-//     //分享到朋友圈
-//     wx.onMenuShareTimeline({
-//         title,   // 分享时的标题
-//         link: fxUrl,     // 分享时的链接
-//         imgUrl: fxImgUrl,    // 分享时的图标
-//         success: function () {
-//             showToast("分享成功");
-//         },
-//         cancel: function () {
-//             showToast("取消分享");
-//         }
-//     });
-//     //分享给朋友
-//     wx.onMenuShareAppMessage({
-//         title,
-//         desc: '就差你啦!助我领取《剑与远征:启程》新春红包封面点击链接[立即预约]完成...', 
-//         link: fxUrl,
-//         imgUrl: fxImgUrl,
-//         type: 'link',
-//         dataUrl: '', 
-//         success: function () {
-//             showToast("分享成功");
-//         },
-//         cancel: function () {
-//             showToast("取消分享");
-//         }
-//     });
-// })
+let { appId, timestamp, nonceStr, signature, title, fxUrl, fxImgUrl, desc } = state;
+console.log(state)
+
+// 分享
+wx.config({
+    debug: false,// 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    appId,        // 必填，公众号的唯一标识，填自己的！
+    timestamp, // 必填，生成签名的时间戳，刚才接口拿到的数据
+    nonceStr,   // 必填，生成签名的随机串
+    signature, // 必填，签名，见附录1
+    jsApiList: [
+    'onMenuShareTimeline',
+    'onMenuShareAppMessage'
+    ]
+})
+
+wx.ready(function () {
+    //分享到朋友圈
+    wx.onMenuShareTimeline({
+        title,   // 分享时的标题
+        link: fxUrl,     // 分享时的链接
+        imgUrl: fxImgUrl,    // 分享时的图标
+        success: function () {
+            showToast("分享成功");
+        },
+        cancel: function () {
+            showToast("取消分享");
+        }
+    });
+    //分享给朋友
+    wx.onMenuShareAppMessage({
+        title,
+        desc, 
+        link: fxUrl,
+        imgUrl: fxImgUrl,
+        type: 'link',
+        dataUrl: '', 
+        success: function () {
+            showToast("分享成功");
+        },
+        cancel: function () {
+            showToast("取消分享");
+        }
+    });
+})
 </script>
 
 <template>
 	<div class="share">
         <div class="share-box">
-            <div class="copy-btn" :data-clipboard-text="text">
+            <div class="copy-btn" :data-clipboard-text="state.text">
                 <img src="@/assets/img/copy-button.png" alt="">
             </div>
         </div>
